d832f25d5954cc091244e9acdaf54a305288916b
Kirk Daniels <kdaniels@unca.edu>
2012-02-04 19:28:21 -0500

    alternating colors

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -8,22 +8,39 @@ function main() {
     //your tasks
 
     //1. Create a spotter and get it to insert tweets into the DOM
-  var s = new Spotter("twitter.search",{q:"terroris", lang:"en"});
+  var s = new Spotter("twitter.search",{q:"terrorism", lang:"en"});
 
   s.register(function(tweets) {
+    var color = 0;
+    var count = 1;
     for(var t in tweets)  {
       var txt = tweets[t].text;
     //2. Add profile images (tweet.profile_image_url)
       var img = tweets[t].profile_image_url;
+    //5. Alternate the colors or the background of the tweets
+      if(color===0) {
+        color = 1;
+        var style = "tweet0";
+      } else {
+        color = 0;
+        var style = "tweet1";
+      }
+      var tweet = $("<p id='"+count+"' class='"+style+"'><img src='"+img+"'/>"+txt+"</p>");
+      tweet.hide();
     //3. Make the tweets occur so the most recent are at the top
-      $("#tweets").prepend("<p><img src='"+img+"'/>"+txt+"</p>");
+      $("#tweets").prepend(tweet);
+    //4. Make the tweets slide down
+      tweet.fadeIn().slideDown();
+      if(count>10){
+        var remove = count-10;
+        $("#"+remove).slideDown().fadeOut();
+      }
+      count = count + 1;
     }
   });
 
   s.start();
 
-    //4. Make the tweets slide down
-    //5. Alternate the colors or the background of the tweets
     //6. Show a maximum of 10 tweets at a time (remove old tweets from the dom)
 
 
